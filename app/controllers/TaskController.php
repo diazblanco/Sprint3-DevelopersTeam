e63ff2596e3e78f3cf991a0f1cc2b2372c7cd7fb
diazblanco <diazblancodesign@gmail.com>
2022-05-12 17:27:17 +0200

    Define roots for each pair of controller/method

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -7,6 +7,9 @@ class TaskController extends ApplicationController{
 		$this->view->message = "hello from task::index";
 		$this->view->render($viewScript);
 		//var_dump(WEB_ROOT);
+		//echo "WEB_ROOT= " . WEB_ROOT ."<br>"; //WEB_ROOT= /phpInitialDemo/web
+        //echo "ROOT_PATH= " . ROOT_PATH ."<br>"; //ROOT_PATH= /Applications/MAMP/htdocs/phpInitialDemo
+        //echo "CMS_PATH= " . CMS_PATH ."<br>"; //CMS_PATH= /Applications/MAMP/htdocs/phpInitialDemo/lib/base/
 	}
 
     //Controlador vista crear tarea
