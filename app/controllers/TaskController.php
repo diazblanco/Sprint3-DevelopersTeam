2d92bd243d436f6a31be2da69659a0fec9e8f900
diazblanco <diazblancodesign@gmail.com>
2022-05-15 12:42:27 +0200

    adding task creation functionality, first try

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
+require ROOT_PATH . '/app/models/ModelTask.php';
+
 class TaskController extends ApplicationController{
 	//Controlador vista principal / index
 	public function indexAction(){
-		require ROOT_PATH . '/app/models/ModelTask.php';
 		$viewScript="header.phtml";
 		$this->view->render($viewScript);
 
@@ -16,7 +17,46 @@ class TaskController extends ApplicationController{
 	public function createAction(){
 		$viewScript="header.phtml";
 		$this->view->render($viewScript);
+		/* $model = new ModelTask();
+		$tasksList = $model->getJsondb();
+		var_dump($tasksList); */
 	}
+
+	//Recoge la info que llega del form para poder enviarla al modelo, que será el que tenga acceso para añadir la nueva tarea a la bbdd
+	public function newAction(){
+		$model = new ModelTask();
+		$tasksList = $model->getJsondb();
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST'){ //Si se está recogiendo la info del form:
+			$form = [
+				'nomtasca' => $_POST['nomtasca'],
+				'nomusuari' => $_POST['nomusuari'],
+				'estattasca' => $_POST['estattasca'],
+				'horainici' => $_POST['horainici'],
+				'horafinal' => $_POST['horafinal'],
+				'id' => '',
+			];
+
+			//Por temas de usabilidad, el id no se lo puedo pedir al usuario, debe introducirlo la máquina de forma automática:
+			//1)Acceder a la última tarea (array) almacenada en la bbdd y luego acceder a la última clave/valor de esa tarea.
+			//2)cojo el valor de la última llave de la tarea y incremento en uno
+			//3)Le asigno ese valor ya incrementado a la clave 'id' del array que genero con lo que llega del form. Así simulo un autoincrement del MySQL
+			//si la bbdd está vacía, le asigno al id el valor 1
+
+			if(isset($tasksList)){ //Si hay datos en la bbdd
+				$lastTask = end($tasksList); //end() avanza el puntero interno dl array hasta su último elemtno y devuelve su valor
+				$lastValueOfTask = $lastTask['id']; //accedo el valor de la última clave del array
+				$form['id'] = $lastValueOfTask++; //asigno a la calve 'id' del array que me llega del form, el valor incrementado en uno que tenía la última id de las tareas ya creadas en la bbdd
+			} else { //si no hay datos en la bbdd, la tarea que cree tendrá id 1
+				$form['id'] = 1; 
+			}
+			
+			$tasksList->addTask($form); //llamo al método del modleo que insertará la nueva tarea a la bbdd
+			header('Location: /phpInitialDemo/web/index');
+		}
+	}
+	
+
 
     //Controlador vista actualizar tarea
 	public function updateAction(){
@@ -25,8 +65,6 @@ class TaskController extends ApplicationController{
 	}
 
 }
-
-
 
 //var_dump(WEB_ROOT);
 //echo "WEB_ROOT= " . WEB_ROOT ."<br>"; //WEB_ROOT= /phpInitialDemo/web
--- app/models/ModelTask.php
+++ app/models/ModelTask.php
@@ -3,6 +3,7 @@
 
 class ModelTask extends Model{
     private $jsondb;
+    private $tasksList;
 
     public function __construct(){
         $this->jsondb = json_decode(file_get_contents(ROOT_PATH . '/config/ddbb.json'),true);
@@ -13,6 +14,23 @@ class ModelTask extends Model{
     //GETTER
     public function getJsondb(){
         return $this->jsondb;
+    }
+
+    //método para convertir la tarea que está en formato array a formato json
+    //json_encode : Retorna la representación json del valor dado
+    //JSON_PRETTY_PRINT : utiliza espacios en blanco para formatear los datos devuetos
+    public function returnJson(){
+        file_put_contents(ROOT_PATH . '/config/ddbb.json', json_encode($tasksList, JSON_PRETTY_PRINT));
+    }
+
+    //añadir tareal al json
+    //Recoger info que llega del form a través del controller
+    public function addTask($form){
+        require ROOT_PATH . '/app/controllers/TaskController.php';
+        $tasksList = $this->getJsondb(); //almaceno el contenido el json en formato array
+
+        $tasksList[] = $form; //añado a la bbdd el array que llega del form
+        $this->returnJson($tasksList); //paso la info en formato array a formato json
     }
 }
 
